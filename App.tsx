e4bdf5c52355d794d04f371b3db5e85af265dad2
Marcos Augusto <marcosagusto1041@gmail.com>
2020-02-25 16:33:34 -0300

    second commit

--- App.tsx
+++ App.tsx
@@ -1,47 +1,84 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import {
+   StyleSheet, Text, View, ScrollView, TouchableOpacity 
+  } from 'react-native'
 import moment from 'moment'
 
-const DATA = {
-  timer: 1232467,
-  lapis: [ 12345, 2345, 34567, 98765],
-}
 
-  function Timer({ interval }) {
+  function Timer({ interval, style }) {
+
     const duration = moment.duration(interval)
     const centiseconds = Math.floor(duration.milliseconds() / 10)
     return ( 
-    <Text style={styles.timer}>
+    <Text style={style}>
         {duration.minutes()}:{duration.seconds()},{centiseconds} 
     </Text>
 
     )
   }
 
-  function RoundButton({  title, color, background }) {
+  function RoundButton({  title, color, background, onPress, disables, disabled  }) {
     return(
-      <View style={[ styles.button, { backgroundColor: background }]}>
+
+      <TouchableOpacity 
+        onPress={() => !disabled && onPress()}
+        style={[ styles.button, { backgroundColor: background }]}
+        activeOpacity={disables ? 1.0 : 0.7}
+      >
         <View style={styles.buttonBorder}>
           <Text style={[ styles.buttonTitle, {color}]}>{title}</Text>
         </View>
-      </View>
+      </TouchableOpacity>
        
     )
   }
 
-  function Lap({ number, interval }) {
-    <View style={styles.lap}> 
-      <Text style={styles.lapText}>Lap {number}</Text>
-      <Text style={styles.lapText}>{interval}</Text>
+  function Lap({ number, interval, fastest, slowest }) {
+
+    const lapStyle = [   
+      styles.lapText,
+      fastest && styles.fastest,
+      slowest && styles.slowest,  
+    ]
+
+    return( 
+      <View style={styles.lap}> 
+
+      <Text style={lapStyle}>Lap {number}</Text>
+      <Timer style={lapStyle} interval={interval}/>
+
     </View>
+
+
+    )
+   
+
   }
 
   function LapsTable({ laps }) {
+    
+    const finishedLaps = laps.slice(1)
+    let min = Number.MAX_SAFE_INTEGER
+    let max = Number.MIN_SAFE_INTEGER 
+    if(finishedLaps.length > 2) {
+      finishedLaps.forEach(lap => {
+        if(lap < min) min = lap
+        if(lap > max) max = lap
+      }); 
+    } 
+     
     return (
     
-        <ScrollView>
+        <ScrollView style={styles.scrollView}>
+        
          {laps.map((lap, index) => (
-           <lap number={index} interval={lap}/>
+           <Lap 
+              number={laps.length - index} 
+              key={laps.length - index} 
+              interval={lap} 
+              fastest={lap === min}
+              slowest={lap === max}
+            />
          ))}
         </ScrollView>
    
@@ -54,21 +91,45 @@ const DATA = {
     )
   }
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.setState = {
+      
+    
+      start: 0,
+      now: 0,
+      laps: [ ],
+    }
+  }
+
+  start = () => {
+
+  }
  
   render() {
 
+    const { now, laps, start } =  this.setState
+    const timer = now - start
+
     return (
       <View style={styles.container}>
-       <Timer interval={DATA.timer}/>
+       <Timer interval={timer} style={styles.timer}/>
 
        <ButtonsRow>
 
         <RoundButton title='Reset' color='#FFFFFF' background='#3D3D3D'/>
-        <RoundButton title='Start' color='#50D167' background='#1B361f'/>
-       
+
+        <RoundButton
+            title='Start' 
+            color='#50D167' 
+            background='#1B361f'
+            onPress={this.start}
+          />
+
        </ButtonsRow>
       
-      
+       <LapsTable laps={laps} />
 
       </View>
     );
@@ -114,8 +175,30 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     justifyContent: 'space-between',
     marginTop: 80,
+    marginBottom: 30,
   },
   lapText: {
     color: '#FFFFFF',
-  }
+    fontSize: 18,
+  },
+  lap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderColor: '#151515',
+    borderTopWidth: 1,
+    paddingVertical: 10,
+  },
+  scrollView: {
+    alignSelf: 'stretch'
+  },
+  fastest: {
+    color: '#4BC05F',
+  },
+  slowest: {
+    color: '#CC3531',
+  },
+  timerContainer: {
+    flexDirection: 'row',
+  },
+ 
 });
